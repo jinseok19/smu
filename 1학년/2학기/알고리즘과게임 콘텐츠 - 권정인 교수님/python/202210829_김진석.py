899d085b35a621babecd1059a3f0de3f9c631bfb
김진석 <121952875+jinseok19@users.noreply.github.com>
2023-01-24 19:43:12 +0900

    상명

--- 1학년/2학기/알고리즘과게임 콘텐츠 - 권정인 교수님/python/202210829_김진석.py
+++
@@ -1,8 +0,0 @@
-chulsu = ['한양아파트', 2, 88, 100, 74]
-younghee = ['단양빌라', 14, 100, 84, 74]
-
-print(chulsu[0]>younghee[0])
-print(chulsu[1]<younghee[1])
-print(chulsu[2]>=younghee[2])
-print(chulsu[3]<=younghee[3])
-print(chulsu[4]!=younghee[4])
